dba41eeb4b7af813c192e3c24e835839d4ea0c2f
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-04-06 08:51:52 +0000

    [Update]リレーションとルーティングとコントローラ再作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,26 @@
 class RelationshipsController < ApplicationController
   def create
-
+    #relationship = current_user.relationships.new(followed_id: user_id)
+    #relationship = Relationship.new(followed_id: user_id)
+    #relationship.user_id = current_user.id
+    #relationship.save
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
 
   def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
 
+  def followings #フォロー一覧viewを表示するアクション
+    user = User.find(params[:user_id])#このparamsを:idにすると？
+    @users = user.followings
+  end
+
+  def followers #フォロワー一覧viewを表示するアクション
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :followings, throuth: :relationships, source: :followed #自分がフォローしている人
-  has_many :followers, through: :reverse_relationships, source: :follower #自分をフォローしている人
+  has_many :followings, through: :follower, source: :followed #自分がフォローしている人
+  has_many :followers, through: :followed, source: :follower #自分をフォローしている人
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -23,15 +23,16 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
+
   def follow(user_id)
-    relationships.create(followed_id: user_id)
+    follower.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+    follower.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
