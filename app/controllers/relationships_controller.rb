faf3d27fe25ad0f4afcd38929b57e5a4269617fd
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-04-07 07:45:39 +0000

    [Update]サンプルコード確認、修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,20 +1,21 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   def create
-    #relationship = current_user.relationships.new(followed_id: user_id)
-    #relationship = Relationship.new(followed_id: user_id)
-    #relationship.user_id = current_user.id
-    #relationship.save
-    current_user.follow(params[:user_id])
+    #current_user.follow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.follow(user)
     redirect_to request.referer
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
+    # current_user.unfollow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
     redirect_to request.referer
   end
 
   def followings #フォロー一覧viewを表示するアクション
-    user = User.find(params[:user_id])#このparamsを:idにすると？
+    user = User.find(params[:user_id])
     @users = user.followings
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :followings, through: :follower, source: :followed #自分がフォローしている人
-  has_many :followers, through: :followed, source: :follower #自分をフォローしている人
+  has_many :followings, through: :relationships, source: :followed #自分がフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower #自分をフォローしている人
 
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -25,12 +25,12 @@ class User < ApplicationRecord
   end
 
 
-  def follow(user_id)
-    follower.create(followed_id: user_id)
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
 
-  def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
