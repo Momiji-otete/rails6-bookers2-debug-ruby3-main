944074d6b843f167c7352a0d68fb2e712dbbdb70
Momiji-otete <126852371+Momiji-otete@users.noreply.github.com>
2023-04-09 08:59:37 +0000

    [Update]コメント機能の非同期通信化の実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,20 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
     # redirect_back(fallback_location: root_path)
     # redirect_to request.referer
+    @book_comment = BookComment.new
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     # redirect_to book_path(params[:book_id])
     # redirect_to request.referer
+    @book = Book.find(params[:book_id])
   end
 
   private
